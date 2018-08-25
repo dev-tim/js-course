bc55d248681008f0a09bc2edd8d39da75e5fceb3
dev-tim <artem.zadorozhniy@gmail.com>
2018-08-25 09:49:30 +0200

    Created intial setup for front-end projects

---
+++ chapter1/index.js
@@ -0,0 +1,21 @@
+(function(){
+
+  console.log('Hello world');
+
+  let blablaFunction = function(phrase) {
+     return function(){
+       console.log('Phrase ' + phrase);
+     }
+  }
+
+
+  let composedBlablaFunction = function(blaFunction){
+    console.log('Get some air in your lungs!');
+    blaFunction();    
+    console.log('Now you can rest!')
+  }
+
+  composedBlablaFunction(blablaFunction('Hello world')) 
+
+
+}());
