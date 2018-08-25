a7fdf1cc84bad4ace52a7da0e7d7caa3afebfc12
dev-tim <artem.zadorozhniy@gmail.com>
2018-08-25 11:48:50 +0200

    Added tests

--- chapter1/index.js
+++ chapter1/index.js
@@ -2,6 +2,21 @@
 
   console.log('Hello world');
 
+  let add = function(a, b){
+    return a + b;
+  }
+
+  let o = {
+     key: { key: { key: 42 }}
+  };
+
+  let arr = [1,2,3,4,5];
+  
+  console.log(arr[4])
+
+
+
+
   let blablaFunction = function(phrase) {
      return function(){
        console.log('Phrase ' + phrase);
