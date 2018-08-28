543b8983e0f76be8e0a3159d17e45484a869ed91
gulya <gulyaluehrs@gmail.com>
2018-08-28 17:40:04 +0200

    fix tests first var

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,10 +1,67 @@
 (function(){
 
-  console.log('Hello world');
+  let Function = {};
 
-  let add = function(a, b){
+  Function.add = function (a, b){
     return a + b;
-  }
+  };
+
+  Function.divideAndMultiply = function (a,b){
+    if (b==0){
+      return undefined;
+    }
+    return a*a/b;
+  };
+
+  Function.roundNumber = function (a){
+    return Math.round;
+  };
+
+  Function.findSinCos = function (a) {
+    return Math.sin(a) * Math.cos(a);
+  };
+
+  Function.parseNumberFromString = function (price){
+    return parseFloat (price);
+  };
+
+  Function.findObjectValueByKey = function  (obj, key){
+    var res = obj ["key"];
+    return res;
+  };
+
+  Function.findObjectValueByDynamicKey = function (obj, key){
+    var res = obj.key;
+    return res;
+  };
+
+  Function.parseObjectFromJSON = function (json){
+    return JSON.parse (json);
+  };
+
+  Function.pushToArray = function (array,b){
+    return array.push(b);
+  };
+
+  Function.popFromArray = function (array,b){
+    return array.pop(b);
+  };
+
+  
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
 
   let o = {
      key: { key: { key: 42 }}
@@ -12,7 +69,7 @@
 
   let arr = [1,2,3,4,5];
   
-  console.log(arr[4])
+  console.log(arr[4]);
 
 
 
@@ -20,14 +77,14 @@
   let blablaFunction = function(phrase) {
      return function(){
        console.log('Phrase ' + phrase);
-     }
-  }
+     };
+  };
 
   let composedBlablaFunction = function(blaFunction) {
     console.log('Get some air in your lungs!');
     blaFunction();    
-    console.log('Now you can rest!')
-  }
+    console.log('Now you can rest!');
+  };
 
-  composedBlablaFunction(blablaFunction('Hello world')) 
+  composedBlablaFunction(blablaFunction('Hello world')) ;
 }());
--- chapter1/test/index.test.js
+++ chapter1/test/index.test.js
@@ -2,84 +2,92 @@ var assert = require('assert');
 
 describe('JS Basics', function() {
   describe('Numbers', function() {
+
+    
     it('should be able to add number', function() {
-      var a = 42.9902822; 
-      var b = 43.2929112;
-      assert.equal(a + b, 99);
+      var a = 42; 
+      var b = 47;
+      assert.equal(add, 99);
     });
 
     it('should be able to divide and multiply number', function() {
-      var a = 42.94; 
+      var a = 42; 
       var b = 0;
-
-      assert.equal(a * a / b, 42);
+      assert.equal(Function.divideAndMultiply(a,b), undefined);
     });
 
     it('should be able to round numbers', function() {
       var a = 42.94; 
-      assert.equal(a, 42);
+      assert.equal(Function.roundNumber (a), 43);
     });
 
     it('should be able to find sin(x) * cos(x)', function() {
       var a = 42;
-      assert.equal(a, 1);
+      assert.equal(Function.findSinCos (a), 0.36659516003);
     });
 
     it('should be able to parse number form string', function() {
-      var price = "9.99 $"
-      assert.equal(price, 9.99);
+      var price = "9.99 $";
+      assert.equal(Function.parseNumberFromString(price), 9.99);
     });
   });
 
 
   describe('Objects', function() {
+    
     it('should be find object value by key', function() {
-      var obj = { a: {b: { d: "foo" }}, c: 42 }
-      assert.equal(obj, "foo");
+      var obj = { a: {b: { d: "foo" }}, c: 42 };
+      assert.equal(Function.findObjectValueByKey(obj, 'd'), "foo");
     });
 
     it('should be find object value by dynamic key', function() {
-      var obj = { a: {b: { d: "foo" }}, c: 42 }
-      assert.equal(obj["hm"], 42);
+      var obj = { a: {b: { d: "foo" }}, c: 42 };
+      assert.equal(Function.findObjectValueByDynamicKey (obj, 'c'), 42);
     });
 
     it('should be parse object from json', function() {
-      var json = '{"ok":true,"user_lessons":[{"user_lesson_id":408097171313,"state":"completed","skip":false,"lesson_id":1,"date_start":1533108640,"tasks":[{"user_task_id":407936828624,"state":"skipped","current_step":"","task_id":1},{"user_task_id":408791535509,"state":"skipped","current_step":"","task_id":2},{"user_task_id":409970847238,"state":"skipped","current_step":"","task_id":3}]}]}'
-      var dateStart = 42;
+      var json = '{"ok":true,"user_lessons":[{"user_lesson_id":408097171313,"state":"completed","skip":false,"lesson_id":1,"date_start":1533108640,"tasks":[{"user_task_id":407936828624,"state":"skipped","current_step":"","task_id":1},{"user_task_id":408791535509,"state":"skipped","current_step":"","task_id":2},{"user_task_id":409970847238,"state":"skipped","current_step":"","task_id":3}]}]}';
+      var parsedJSON = Function.parseObjectFromJSON (json);
+      var dateStart = parsedJSON.user_lessons[0].date_start;
       assert.equal(dateStart, 1533108640);
     });
 
-    it('should be set objet key', function() {
-      var obj = { a: {b: { d: "foo" }}, c: 42 }
-      assert.equal(obj.a.b, "Js Rocks!")
+    it('should be set object key', function() {
+      var obj = { a: {b: { d: "foo" }}, c: 42 };
+      obj.a.b = "Js Rocks!";
+      assert.equal(obj.a.b, "Js Rocks!");
     });
   });
 
 
   describe('Arrays', function() {
+    
     it('should be access array by index', function() {
-      var arrray = [1,2,3,4,5,6,7,8,9]
-      assert.equal(arrray, 5);
+      var arrray = [1,2,3,4,5,6,7,8,9];
+      assert.equal(arrray [4], 5);
     });
 
     it('should to push and pop from array', function() {
-      var arrray = [1,2,3,4,5,6,7,8,9]
-      assert.equal(arrray.length, 10);
+      var array = [1,2,3,4,5,6,7,8,9];
+      var array1 = Function.pushToArray (array, 10, 11, 12); 
+      var array2 = Function.popFromArray (array1, 12);
+      assert.equal(array2.length, 10);
     });
 
     it('should be able to output square of array values', function() {
-      assert.equal()
+      var array = [1,2,3,4,5,6,7,8,9];
+      var exponent = 2;
+      assert.equal(Function.squareOfArrayValue ((array[3],exponent), 16));
     });
 
     it('should be able to sort array', function() {
-      var arr = [23,23,4,5,123,7,32,13,13,9]
-      assert.equal(arr, []);
+      var arr = [7, 15, 2, 48];
+      assert.equal(Function.sortArray (arr), [2, 7, 15, 48]);
     });
 
     it('should be able to reverse string', function() {
-
-      var string  = "I love corgies!"
-      assert.equal(string, "!seigroc evol I")
+      var string  = "I love corgies!";
+      assert.equal(Function.reverseString (string), "!seigroc evol I");
     });
   });
 });
