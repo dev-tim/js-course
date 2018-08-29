64149045b8c9a2b47fae2f4b4e394aa2c66d74a3
dev-tim <artem.zadorozhniy@gmail.com>
2018-08-29 10:20:42 +0200

    added bank account

---
+++ chapter1/test/bank-account.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var BankAccount = require('../bank-account').BankAccount;
+
+describe('Bank account', () => {
+    it('should have no money on deposit in the beginning', () => {
+
+        var obj = {
+            foo: "hello world",
+            fn: function () {
+                console.log(this.foo.split(''));
+            }
+        }
+
+        var boo = obj.fn;
+        console.log(boo());
+        //assert.equal(BankAccount.balance, 0);
+    });
+})
