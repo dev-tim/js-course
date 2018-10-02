ce8086da2d90440cf31ca0b8a6029b02ab152d6c
dev-tim <artem.zadorozhniy@gmail.com>
2018-10-02 19:08:34 +0200

    Addded front-end app setup

--- chapter1/test/bank-account.test.js
+++ chapter1/test/bank-account.test.js
@@ -7,12 +7,12 @@ describe('Bank account', () => {
         var obj = {
             foo: "hello world",
             fn: function () {
-                console.log(this.foo.split(''));
+                console.log(arguments.join(''));
             }
         }
 
-        var boo = obj.fn;
-        console.log(boo());
+        obj.fn(1,2,3,4,45,6, "", {}, [], Infinity,1,23,1,12,12,3,12,3123,123,123,123,123,123,123);
+        
         //assert.equal(BankAccount.balance, 0);
     });
 })
