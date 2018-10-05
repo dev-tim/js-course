77ebaf6acd3391399e312a8a1da9b116ed48af7f
dev-tim <artem.zadorozhniy@gmail.com>
2018-10-05 22:43:08 +0200

    Add more interesting version of the homework

---
+++ chapter4-homework/public/scripts/app.js
@@ -0,0 +1,11 @@
+(function(){
+    // add here following boostrap elements
+
+    // success button
+    // http://getbootstrap.com/docs/4.1/components/buttons/
+
+    // login form
+    // http://getbootstrap.com/docs/4.1/components/forms/
+
+    // card
+})();
