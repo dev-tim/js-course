d843bc0ce71df66f5b88fa9eb1bc25f464d35fe7
KRdoubleL <angrybanker1@gmail.com>
2018-10-09 20:54:29 +0200

    s

--- chapter5/lesson.js
+++ chapter5/lesson.js
@@ -11,5 +11,18 @@
 
     window.addEventListener('mousemove', function(e){
         console.log([e.clientX, e.clientY]);
-    })
+    });
+
+    debugger;
+    var coordinates = document.querySelector('#coordinates');
+    var xCoordinateLabel = document.createElement('span');
+    xCoordinateLabel.innerText = ' X coordinate';
+
+    debugger;
+    var yCoordinateLabel = document.createElement('span');
+    yCoordinateLabel.innerText = ' Y coordinate';
+
+    debugger;
+    coordinates.appendChild(xCoordinateLabel);
+    coordinates.appendChild(yCoordinateLabel);
 })();
