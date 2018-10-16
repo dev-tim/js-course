99ecc3601f498e57633a207790ffbeb5a2446f5e
dev-tim <artem.zadorozhniy@gmail.com>
2018-10-16 20:45:01 +0200

    Chapter 6

---
+++ chapter6/public/scripts/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "/users",
+    }).done(function(data) {
+        $('#loading-element').hide();
+        debugger;
+    });
+
+    $('#loading-element').show();
+})
