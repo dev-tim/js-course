3e0c58c088242828b248b05488584caf357b7540
dev-tim <artem.zadorozhniy@gmail.com>
2018-10-17 23:38:08 +0200

    Chapter 6

--- chapter6/public/scripts/app.js
+++ chapter6/public/scripts/app.js
@@ -1,9 +1,43 @@
+/* Nano Templates - https://github.com/trix/nano */
+
+function nano(template, data) {
+    return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+      var keys = key.split("."), v = data[keys.shift()];
+      for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+      return (typeof v !== "undefined" && v !== null) ? v : "";});
+}
+
+const TEMPLATE = "<div class=\"row list-item\">\n" +
+"                <div class=\"col-4\">\n" +
+"                    {name}\n" +
+"                </div>\n" +
+"                 <div class=\"col-4\">\n" +
+"                    {email}\n" +
+"                </div>\n" +
+"                <div class=\"col-2\">\n" +
+"                    {type}\n" +
+"                </div>\n" +
+"\n" +
+"                <div class=\"col-2\">\n" +
+"                    <button class=\"btn btn-success\">Edit</button>\n" +
+"                </div>\n" +
+"            </div>";
+
 $(document).ready(function(){
     $.ajax({
         url: "/users",
     }).done(function(data) {
         $('#loading-element').hide();
         debugger;
+        
+        let result = "";
+        data.map(function(user){
+            const renderedString = nano(TEMPLATE, user);
+            result = result + renderedString;
+        })
+
+        const dynamicUserElement = $(result);
+        $('#item-list').append(dynamicUserElement);        
     });
 
     $('#loading-element').show();
--- chapter6/server.js
+++ chapter6/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const users = [
@@ -13,12 +14,26 @@ const users = [
         name: 'Vasya',
         email: 'vasya@gmail.com',
         type: 'teacher'
-    }
+    },
+    {
+        id: 3,
+        name: 'Lena',
+        email: 'lena@gmail.com',
+        type: 'student'
+    },
 ];
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.get('/users', function(req, res){
+    res.send(users);
+})
+
+app.post('/users', function(req, res){
+    const user = req.body;
+    console.log('Req' + JSON.stringify(req.body))
+    users.push(user);
     res.send(users);
 })
 
