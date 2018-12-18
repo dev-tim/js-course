025052dc34bd47aae5d4d7007fa2f2a6dc7f5660
dev-tim <artem.zadorozhniy@gmail.com>
2018-12-18 20:25:38 +0100

    Created auth sample

---
+++ chapter11-oauth/index.js
@@ -0,0 +1,54 @@
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+let apiUrl = 'https://slack.com/api';
+
+const getUserFullname = (team, user) => new Promise((resolve, reject) => {
+  let oauthToken = storage.getItemSync(team);
+  console.log(oauthToken);
+  request.post('https://slack.com/api/users.info', {form: {token: oauthToken, user: user}}, function (error, response, body) {
+    if (!error && response.statusCode < 300) {
+      console.log(body);
+      return resolve(JSON.parse(body).user.real_name);
+    } else {
+      return resolve('Error occured! ', error);
+    }
+  });
+});
+
+app.get('/auth', function(req, res){
+  if (!req.query.code) { 
+    console.log('Access denied');
+    return;
+  }
+  
+  var data = {form: {
+    client_id: '407936343120.446070522853',
+    client_secret: '2e2588e3738da7d8439033ce9896c1ec',
+    code: req.query.code
+  }};
+
+  request.post(apiUrl + '/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send({ data: body })
+    }
+  })
+});
+
+app.get('/team/:id', function (req, res) {
+
+});
